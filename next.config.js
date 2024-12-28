0567a90399cbb34621fce8acef4a2675c04c51d0
LynchNode <99635034+lynchnode@users.noreply.github.com>
2024-12-29 05:48:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ const nextConfig = {
 	reactStrictMode: true
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: "./",
+  output: 'export',  // Enables static HTML export
+  images: {
+    unoptimized: true // Required for static export
+  }
+}
